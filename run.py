49f2020b9368f7820ebdcc68ef897a9c11bdf05a
one1189 <>
2023-06-08 05:44:01 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+countries = ['Denmark', 'Malaysia', 'Egypt', 'Luxembourg', 'New Zealand']
+technology = ['Playstation', 'Polaroid', 'Samsung', 'Microsoft', 'Google']
+food_and_drink = ['Mozzarella', 'Baguette', 'Apple', 'Tea', 'Physalis']
+
+def start_game():
+    """
+    Welcomes user and asks for a category to begin game
+    """
+    
+    print("Welcome to Hangman\n")
+    name = input("Please Enter your Name: \n")
+    print(f"Welcome {name}.\n")
+    category = input("Please Select a category. Countries(c), Technology(t) or Food and Drink(f)\n")
+    
+    if category != "c":
+        print("Invalid entry, please select a category\n")
+
+start_game()
