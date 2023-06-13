6d60f482ecf276b0f8249fa0ddc2b11d85ef3af5
one1189 <>
2023-06-13 05:30:31 +0000

    create python file for country

--- run.py
+++ run.py
@@ -2,17 +2,17 @@ countries = ['Denmark', 'Malaysia', 'Egypt', 'Luxembourg', 'New Zealand']
 technology = ['Playstation', 'Polaroid', 'Samsung', 'Microsoft', 'Google']
 food_and_drink = ['Mozzarella', 'Baguette', 'Apple', 'Tea', 'Physalis']
 
-def start_game():
+def welcome_player():
     """
     Welcomes user and asks for a category to begin game
     """
-    
+
     print("Welcome to Hangman\n")
-    name = input("Please Enter your Name: \n")
+    name = input("Please Enter your Name: \n").capitalize()
     print(f"Welcome {name}.\n")
-    category = input("Please Select a category. Countries(c), Technology(t) or Food and Drink(f)\n")
+    category = input("Please Select a category.\n Countries(C), Technology(T) or Food and Drink(F)\n").upper()
     
     if category != "c":
         print("Invalid entry, please select a category\n")
 
-start_game()
+welcome_player()
