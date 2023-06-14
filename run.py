c2e6dd7dd10c48e3623696a63260e4b6a968bf23
one1189 <>
2023-06-14 08:58:26 +0000

    Update welcome player function

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ import food_and_drink
 import technology
 
 
-
 def welcome_player():
     """
     Welcomes user and asks for a category to begin game
@@ -14,21 +13,20 @@ def welcome_player():
     print("Welcome to Hangman\n")
     name = input("Please Enter your Name: ").capitalize()
     print(f"Welcome {name}.\n")
-    
-    user_choice = input("Please Select a category.\n 1. Countries\n 2. Technology\n 3. Food and Drink\n")
-    category = ""
-    
-    if user_choice == 1:
-        category = "Countries"
-    elif user_choice == 2:
-        category = "Technology"
-    elif user_choice == 3:
-        category = "Food and Drink"
-    else:
-        print("Invalid selection. Please select a category 1 - 3.")
-    
-    input(f"{name}, you have selected {category}. Do you wish to continue with this selection? Y/N ").upper()
-    
-    
+
+    while True:
+        try:
+            user_choice = int(input(
+                "Please Select a category.\n 1. Countries\n 2. Technology\n 3. Food and Drink\n"))
+            if user_choice >= 1 and user_choice <= 3:
+                print("Valid Selection")
+                break
+            else:
+                print("Invalid Entry. Please select a category.\n")
+        except ValueError:
+            print("Please select a number between 1 and 3.\n")
+
+    decision = input(f"{name}, you have selected {user_choice}. Do you wish to continue? Y/N ").upper()
+
 
 welcome_player()
