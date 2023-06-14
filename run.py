74c6d93948cc428c069c71cf980a34b90d13da4b
one1189 <>
2023-06-14 13:33:21 +0000

    Update select category function

--- run.py
+++ run.py
@@ -5,63 +5,58 @@ from food_and_drink import fd_list
 from technology import tech_list
 from gallows import art
 
-name = None
+
 
 def welcome_player():
     """
-    Welcomes the user
+    Welcomes the user, asks to select a category and 
+    checks whether they are happy with their decision
     """
 
     global name
 
     print("Welcome to Hangman\n")
     name = input("Please Enter your Name: ").capitalize()
+
+    while True:
+        category = select_category()
+
+        decision = input(f"{name}, you have selected {category}. Are you happy with your choice? (Y/N) ").upper()
+
+        if decision == "Y":
+            print("Let's play. Good luck!\n")
+            break
+        elif decision == "N":
+            print("Please select another category.\n")
+        else:
+            print("Invalid input. Please enter 'Y' or 'N'.\n")
     
     print (f"Welcome {name}. You will only have six attempts to correctly guess the word.\n")
 
 
-def select_category()
+def select_category():
+    """
+    Prompts the user to select a category from a list.
+    """
 
     while True:
         try:
-            user_choice = int(input(
-                "Please Select a category.\n 1. Countries\n 2. Technology\n 3. Food and Drink\n"))
-            if user_choice >= 1 and user_choice <= 3:
-                print("Valid Selection")
+            selection = int(input("Please select a category:\n1. Countries\n2. Technology\n3. Food and Drink\n"))
+            if 1 <= selection <= 3:
                 break
             else:
-                print("Invalid Entry. Please select a category.\n")
+                print("Invalid entry. Please select a valid category.\n")
         except ValueError:
-            print("Please select a number between 1 and 3.\n")
+            print("Please enter a number between 1 and 3.\n")
 
-    selection = ""
-
-    if user_choice == 1:
-        selection = "Countries"
-    elif user_choice == 2:
-        selection = "Technology"
+    if selection == 1:
+        category = "Countries"
+    elif selection == 2:
+        category = "Technology"
     else:
-        selection = "Food and Drink"
+        category = "Food and Drink"
 
-    while True:
-        try:
-            decision = str(input(
-                f"{name}, you have selected {selection}. Do you wish to continue? Y/N ")).upper()
-            if decision == "Y":
-                print("Let's Play. Good luck\n")
-                break
-            elif decision == "N":
-                return user_choice
-            else:
-                print("You must select either y or n")
-        except ValueError:
-            print("Invalid character. Please try again")
-
-
-def hangman_game():
-    """
-    Begins the game of hangman
-    """
+    return category
 
 
 welcome_player()
